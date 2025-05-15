2500de6c54edcc8dfd8373f0a2c946cbf9873be4
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-05-15 23:24:12 +0530

    Perfectly adding AccessToken

--- backend/models/google.modal.js
+++ backend/models/google.modal.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, },
-    email: { type: String, },
-    image: { type: String, },
+    name: { type: String },
+    email: { type: String },
+    image: { type: String },
+    accessToken: { type: String },
   },
   { timestamps: true }
 );
