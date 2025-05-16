6cdaa7f974cefb2e3eee92efd6b40eb93482145c
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-05-16 15:38:20 +0530

    Adding ThirdParty OAuth Also -Future Development

---
+++ frontend/src/components/ThirdPartyAuth/ThirdPartyOAuth.jsx
@@ -0,0 +1,8 @@
+// src/App.jsx
+export default function App() {
+  return (
+    <div className="h-screen w-screen bg-black flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">Hello World</h1>
+    </div>
+  );
+}
