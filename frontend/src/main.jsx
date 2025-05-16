ad9980b42d13cb712269747164c463cc974707da
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-05-16 20:43:21 +0530

    Adding AuthProvider and wrap the component over it

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,15 +15,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </Auth0Provider>
   </React.StrictMode>
 );
-
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
